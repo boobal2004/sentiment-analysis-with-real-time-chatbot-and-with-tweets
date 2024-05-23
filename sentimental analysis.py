063d8c43a053bfdaca0ae9bc50215fe12ff3176f
boobal2004 <boobal4002@gmail.com>
2024-05-23 14:32:26 +0530

    Add files via upload

---
+++ sentimental analysis.py
@@ -0,0 +1,561 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import re
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+from textblob import TextBlob
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english'))
+from wordcloud import WordCloud
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, ConfusionMatrixDisplay
+
+
+# In[60]:
+
+
+df = pd.read_csv('vaccination_tweets.csv')
+
+
+# In[61]:
+
+
+df.head()
+
+
+# In[62]:
+
+
+df.info()
+
+
+# In[63]:
+
+
+df.isnull().sum()
+
+
+# In[64]:
+
+
+df.columns
+
+
+# In[65]:
+
+
+text_df = df.drop(['id', 'user_name', 'user_location', 'user_description', 'user_created',
+       'user_followers', 'user_friends', 'user_favourites', 'user_verified',
+       'date', 'hashtags', 'source', 'retweets', 'favorites',
+       'is_retweet'], axis=1)
+text_df.head()
+
+
+# In[66]:
+
+
+print(text_df['text'].iloc[0],"\n")
+print(text_df['text'].iloc[1],"\n")
+print(text_df['text'].iloc[2],"\n")
+print(text_df['text'].iloc[3],"\n")
+print(text_df['text'].iloc[4],"\n")
+
+
+# In[67]:
+
+
+text_df.info()
+
+
+# In[68]:
+
+
+def data_processing(text):
+    text = text.lower()
+    text = re.sub(r"https\S+|www\S+https\S+", '',text, flags=re.MULTILINE)
+    text = re.sub(r'\@w+|\#','',text)
+    text = re.sub(r'[^\w\s]','',text)
+    text_tokens = word_tokenize(text)
+    filtered_text = [w for w in text_tokens if not w in stop_words]
+    return " ".join(filtered_text)
+
+
+# In[69]:
+
+
+text_df.text = text_df['text'].apply(data_processing)
+
+
+# In[70]:
+
+
+text_df = text_df.drop_duplicates('text')
+
+
+# In[71]:
+
+
+stemmer = PorterStemmer()
+def stemming(data):
+    text = [stemmer.stem(word) for word in data]
+    return data
+
+
+# In[72]:
+
+
+text_df['text'] = text_df['text'].apply(lambda x: stemming(x))
+
+
+# In[73]:
+
+
+text_df.head()
+
+
+# In[74]:
+
+
+print(text_df['text'].iloc[0],"\n")
+print(text_df['text'].iloc[1],"\n")
+print(text_df['text'].iloc[2],"\n")
+print(text_df['text'].iloc[3],"\n")
+print(text_df['text'].iloc[4],"\n")
+
+
+# In[75]:
+
+
+text_df.info()
+
+
+# In[76]:
+
+
+def polarity(text):
+    return TextBlob(text).sentiment.polarity
+
+
+# In[77]:
+
+
+text_df['polarity'] = text_df['text'].apply(polarity)
+
+
+# In[78]:
+
+
+text_df.head(10)
+
+
+# In[79]:
+
+
+def sentiment(label):
+    if label <0:
+        return "Negative"
+    elif label ==0:
+        return "Neutral"
+    elif label>0:
+        return "Positive"
+
+
+# In[80]:
+
+
+text_df['sentiment'] = text_df['polarity'].apply(sentiment)
+
+
+# In[81]:
+
+
+text_df.head()
+
+
+# In[82]:
+
+
+fig = plt.figure(figsize=(5,5))
+sns.countplot(x='sentiment', data = text_df)
+
+
+# In[83]:
+
+
+fig = plt.figure(figsize=(7,7))
+colors = ("yellowgreen", "gold", "red")
+wp = {'linewidth':2, 'edgecolor':"black"}
+tags = text_df['sentiment'].value_counts()
+explode = (0.1,0.1,0.1)
+tags.plot(kind='pie', autopct='%1.1f%%', shadow=True, colors = colors,
+         startangle=90, wedgeprops = wp, explode = explode, label='')
+plt.title('Distribution of sentiments')
+
+
+# In[84]:
+
+
+pos_tweets = text_df[text_df.sentiment == 'Positive']
+pos_tweets = pos_tweets.sort_values(['polarity'], ascending= False)
+pos_tweets.head()
+
+
+# In[85]:
+
+
+text = ' '.join([word for word in pos_tweets['text']])
+plt.figure(figsize=(20,15), facecolor='None')
+wordcloud = WordCloud(max_words=500, width=1600, height=800).generate(text)
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.title('Most frequent words in positive tweets', fontsize=19)
+plt.show()
+
+
+# In[86]:
+
+
+neg_tweets = text_df[text_df.sentiment == 'Negative']
+neg_tweets = neg_tweets.sort_values(['polarity'], ascending= False)
+neg_tweets.head()
+
+
+# In[87]:
+
+
+text = ' '.join([word for word in neg_tweets['text']])
+plt.figure(figsize=(20,15), facecolor='None')
+wordcloud = WordCloud(max_words=500, width=1600, height=800).generate(text)
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.title('Most frequent words in negative tweets', fontsize=19)
+plt.show()
+
+
+# In[88]:
+
+
+neutral_tweets = text_df[text_df.sentiment == 'Neutral']
+neutral_tweets = neutral_tweets.sort_values(['polarity'], ascending= False)
+neutral_tweets.head()
+
+
+# In[89]:
+
+
+text = ' '.join([word for word in neutral_tweets['text']])
+plt.figure(figsize=(20,15), facecolor='None')
+wordcloud = WordCloud(max_words=500, width=1600, height=800).generate(text)
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.title('Most frequent words in neutral tweets', fontsize=19)
+plt.show()
+
+
+# In[90]:
+
+
+vect = CountVectorizer(ngram_range=(1,2)).fit(text_df['text'])
+
+
+# In[91]:
+
+
+feature_names = vect.get_feature_names_out()
+print("Number of features: {}\n".format(len(feature_names)))
+print("First 20 features:\n {}".format(feature_names[:20]))
+
+
+# In[92]:
+
+
+X = text_df['text']
+Y = text_df['sentiment']
+X = vect.transform(X)
+
+
+# In[93]:
+
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+
+# In[94]:
+
+
+print("Size of x_train:", (x_train.shape))
+print("Size of y_train:", (y_train.shape))
+print("Size of x_test:", (x_test.shape))
+print("Size of y_test:", (y_test.shape))
+
+
+# In[95]:
+
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# In[96]:
+
+
+logreg = LogisticRegression()
+logreg.fit(x_train, y_train)
+logreg_pred = logreg.predict(x_test)
+logreg_acc = accuracy_score(logreg_pred, y_test)
+print("Test accuracy: {:.2f}%".format(logreg_acc*100))
+
+
+# In[97]:
+
+
+print(confusion_matrix(y_test, logreg_pred))
+print("\n")
+print(classification_report(y_test, logreg_pred))
+
+
+# In[98]:
+
+
+style.use('classic')
+cm = confusion_matrix(y_test, logreg_pred, labels=logreg.classes_)
+disp = ConfusionMatrixDisplay(confusion_matrix = cm, display_labels=logreg.classes_)
+disp.plot()
+
+
+# In[99]:
+
+
+from sklearn.model_selection import GridSearchCV
+
+
+# In[100]:
+
+
+param_grid={'C':[0.001, 0.01, 0.1, 1, 10]}
+grid = GridSearchCV(LogisticRegression(), param_grid)
+grid.fit(x_train, y_train)
+
+
+# In[101]:
+
+
+print("Best parameters:", grid.best_params_)
+
+
+# In[102]:
+
+
+y_pred = grid.predict(x_test)
+
+
+# In[103]:
+
+
+logreg_acc = accuracy_score(y_pred, y_test)
+print("Test accuracy: {:.2f}%".format(logreg_acc*100))
+
+
+# In[104]:
+
+
+print(confusion_matrix(y_test, y_pred))
+print("\n")
+print(classification_report(y_test, y_pred))
+
+
+# In[105]:
+
+
+style.use('classic')
+cm = confusion_matrix(y_test, y_pred, labels=logreg.classes_)
+disp = ConfusionMatrixDisplay(confusion_matrix = cm, display_labels=logreg.classes_)
+disp.plot()
+
+
+# In[106]:
+
+
+from sklearn.svm import LinearSVC
+
+
+# In[107]:
+
+
+SVCmodel = LinearSVC()
+SVCmodel.fit(x_train, y_train)
+
+
+# In[108]:
+
+
+svc_pred = SVCmodel.predict(x_test)
+svc_acc = accuracy_score(svc_pred, y_test)
+print("test accuracy: {:.2f}%".format(svc_acc*100))
+
+
+# In[109]:
+
+
+print(confusion_matrix(y_test, svc_pred))
+print("\n")
+print(classification_report(y_test, svc_pred))
+
+
+# In[110]:
+
+
+style.use('classic')
+cm = confusion_matrix(y_test, svc_pred, labels=logreg.classes_)
+disp = ConfusionMatrixDisplay(confusion_matrix = cm, display_labels=logreg.classes_)
+disp.plot()
+
+
+# In[111]:
+
+
+grid = {
+    'C':[0.01, 0.1, 1, 10],
+    'kernel':["linear","poly","rbf","sigmoid"],
+    'degree':[1,3,5,7],
+    'gamma':[0.01,1]
+}
+grid = GridSearchCV(SVCmodel, param_grid)
+grid.fit(x_train, y_train)
+
+
+# In[112]:
+
+
+print("Best parameter:", grid.best_params_)
+
+
+# In[113]:
+
+
+y_pred = grid.predict(x_test)
+
+
+# In[114]:
+
+
+logreg_acc = accuracy_score(y_pred, y_test)
+print("Test accuracy: {:.2f}%".format(logreg_acc*100))
+
+
+# In[115]:
+
+
+print(confusion_matrix(y_test, y_pred))
+print("\n")
+print(classification_report(y_test, y_pred))
+
+
+# In[116]:
+
+
+style.use('classic')
+cm = confusion_matrix(y_test, y_pred, labels=logreg.classes_)
+disp = ConfusionMatrixDisplay(confusion_matrix = cm, display_labels=logreg.classes_)
+disp.plot()
+
+
+# In[117]:
+
+
+pip install vadersentiment
+
+
+# In[118]:
+
+
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+def analyze_sentiment(user_input):
+    # Create a VADER sentiment analyzer object
+    analyzer = SentimentIntensityAnalyzer()
+
+    # Get sentiment scores
+    sentiment_scores = analyzer.polarity_scores(user_input)
+
+    # Determine the sentiment label based on the compound score
+    sentiment_score = sentiment_scores['compound']
+
+    if sentiment_score >= 0.05:
+        sentiment_label = 'POSITIVE😁'
+    elif sentiment_score <= -0.05:
+        sentiment_label = 'NEGATIVE😭'
+    else:
+        sentiment_label = 'NEUTRAL🙂'
+
+    return sentiment_label, sentiment_score
+
+def chatbot():
+    print("Chatbot: Hi there! I'm your friendly chatbot. How can I assist you today?")
+
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() in ['exit', 'bye', 'quit']:
+            print("Chatbot: Goodbye! Have a great day!")
+            break
+        sentiment_label, sentiment_score = analyze_sentiment(user_input)
+
+        print(f"Chatbot: Sentiment: {sentiment_label}, Score: {sentiment_score:.4f}")
+
+if __name__ == "__main__":
+    chatbot()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
